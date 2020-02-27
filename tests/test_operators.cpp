7d5a8501811151782723c9dba9878ae3c531c605
Emmanuel <emampons@gmail.com>
2020-02-27 14:44:18 -0500

    fixed operator test

--- tests/test_operators.cpp
+++ tests/test_operators.cpp
@@ -7,140 +7,81 @@
 #include "../src/leodb/value.h"
 #include "../src/leodb/entry.h"
 #include "../src/leodb/entry.cpp"
+#include "gtest/gtest.h"
 
 
-bool driver(){
-    // Create a DB object
+class OperatorsTest : public ::testing::Test {
+protected:
     DB<int, int> db = DB<int, int>();
-    bool ret = true;
 
-    std::cout << "[INFO] Testing .put() function...\n";
-    try {
-        for (int x = 0; x < 100; x++){
-            db.put(Key<int>(x), Value<int>(x+100));
-        };
-        int size = db.size();
-        bool passed = (size == 100);
-        if (!passed) {
-            std::cout << "[FAILED] Failed on put function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on put function\n";
-        ret = false;
-    };
-
-    std::cout << "[INFO] Testing .get() function...\n";
-    try {
-        int value = db.get(Key<int>(99)).getItem();
-        bool passed = (value == 199);
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .get() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .get() function\n";
-        ret = false;
-    }
-
-    std::cout << "[INFO] Testing .min() and .max() function...\n";
-    try {
-        int min_keys = db.min();
-        int max_keys = db.max();
-        int min_value = db.min(false);
-        int max_value = db.max(false);
-        bool passed_keys = (min_keys == 0 && max_keys == 99);
-        bool passed_value = (min_value == 100 && max_value == 199);
-        bool passed = (passed_keys && passed_value);
-
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .min() or .max() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .min() or .max() function\n";
-        ret = false;
-    }
-
-    std::cout << "[INFO] Testing .avg() function...\n";
-    try {
-        float avg_keys = db.avg();
-        float avg_values = db.avg(false);
-        bool passed = (avg_keys == 49.5 && avg_values == 149.5);
-
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .avg() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .avg() function\n";
-        ret = false;
-    }
-
-    std::cout << "[INFO] Testing .stddev() function...\n";
-    try {
-        std::string stddev_keys = std::to_string(db.stddev());
-        std::string stddev_values = std::to_string(db.stddev(false));
-        bool passed = (stddev_keys.compare("29.011492") == 0 && stddev_values.compare("29.011492") == 0);
-
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .stddev() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .stddev() function\n";
-        ret = false;
-    }
-
-    std::cout << "[INFO] Testing .scan() function...\n";
-    try {
-        std::vector<Value<int> > response = db.scan(Key<int>(10), Key<int>(20));
-        int start = 120;
-        bool passed = true;
-        for (auto value: response) {
-            if (value.getItem() != start) {
-                passed = false;
-            }
-            start--;
+    void SetUp() override {
+        for (int x = 0; x < 100; x++) {
+            db.put(Key<int>(x), Value<int>(x + 100));
         }
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .scan() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .scan() function\n";
-        ret = false;
     }
+};
 
-    std::cout << "[INFO] Testing .delete() function...\n";
-    try {
-        for (int x = 20; x <= 40; x++) {
-            db.del(Key<int>(x));
-        }
-        int size = db.size();
-        bool passed = (size == 79);
 
-        if (!passed) {
-            std::cout << "[FAILED] Failed on .delete() function\n";
-            ret = false;
-        };
-    } catch(int e) {
-        std::cout << "[FAILED] Failed on .delete() function\n";
-        ret = false;
-    }
-
-    return ret;
+TEST_F(OperatorsTest, IsPutWorking) {
+    EXPECT_EQ(db.size(), 100);
 }
 
 
+TEST_F(OperatorsTest, GetFunctionality) {
+    int value = db.get(Key<int>(99)).getItem();
 
-int main() {
-    bool ret = driver();
-    if (ret) {
-        std::cout << "[INFO] Tests have passed! :)\n";
-        return 0;
-    } else {
-        std::cout << "[INFO] Tests have Failed! :(\n";
-        return 1;
+    EXPECT_EQ(value, 199);
+}
+
+
+TEST_F(OperatorsTest, MinMaxFunctionality) {
+    int min_keys = db.min();
+    int max_keys = db.max();
+    int min_value = db.min(false);
+    int max_value = db.max(false);
+
+
+    EXPECT_EQ(min_keys, 0);
+    EXPECT_EQ(max_keys, 99);
+    EXPECT_EQ(min_value, 100);
+    EXPECT_EQ(max_value, 199);
+}
+
+
+TEST_F(OperatorsTest, AverageFunctionality) {
+    float avg_keys = db.avg();
+    float avg_values = db.avg(false);
+    EXPECT_EQ(avg_keys, 49.5);
+    EXPECT_EQ(avg_values, 149.5);
+}
+
+TEST_F(OperatorsTest, StdDevFunctionality) {
+    std::string stddev_keys = std::to_string(db.stddev());
+    std::string stddev_values = std::to_string(db.stddev(false));
+
+    EXPECT_EQ(stddev_keys, "29.011492");
+    EXPECT_EQ(stddev_values, "29.011492");
+}
+
+TEST_F(OperatorsTest, ScanFunctionality) {
+    std::vector<Value<int> > response = db.scan(Key<int>(10), Key<int>(20));
+    std::vector<Value<int> > correct_range;
+    for(int x = 120; x >=110; x--){
+        correct_range.emplace_back(x);
     }
-};
+    EXPECT_EQ(response, correct_range);
+}
+
+TEST_F(OperatorsTest, DeleteFuncitonality) {
+    for (int x = 20; x <= 40; x++) {
+        db.del(Key<int>(x));
+    }
+    int size = db.size();
+    EXPECT_EQ(size, 79);
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
